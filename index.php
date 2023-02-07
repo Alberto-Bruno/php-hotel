d2d38a7ba7771bab93d2d84a9d7bcd784f401edf
Alberto-Bruno <brunoalberto97@outlook.it>
2023-02-07 23:13:21 +0100

    Hotels available

--- index.php
+++ index.php
@@ -50,21 +50,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
-    <!-- Bootstrap-->
+    <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
-    <!-- Font Awesome-->
+    <!-- Font Awesome -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.css'
         integrity='sha512-FA9cIbtlP61W0PRtX36P6CGRy0vZs0C2Uw26Q1cMmj3xwhftftymr0sj8/YeezDnRwL9wtWw8ZwtCiTDXlXGjQ=='
         crossorigin='anonymous'>
+
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>
     <div class="container">
         <div class='d-flex flex-wrap justify-content-center'>
-            <h3 class="mt-4">Hotel Disponibili</h3>
+            <h3 class="mt-4">Hotels available</h3>
             <table class='table text-capitalize'>
                 <thead>
                     <tr>
@@ -75,6 +78,19 @@
                         <th class='col'>distance of center</th>
                     </tr>
                 </thead>
+                <?php foreach($hotels as $hotel) { 
+                        $park=$hotel['parking']? '<i class="fas fa-circle-check"></i>' : '<i class="fas fa-circle-xmark"></i>';
+                    ?>
+                <tr>
+                    <td><?php echo $hotel['name']; ?></td>
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $park; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?> Kms </td>
+                </tr>
+                <?php
+                    }
+                    ?>
             </table>
         </div>
     </div>
